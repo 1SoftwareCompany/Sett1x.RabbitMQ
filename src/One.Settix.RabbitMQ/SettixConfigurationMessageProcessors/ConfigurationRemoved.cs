d58c8483689b29826017141468c71bd486d09565
zhorodimitrov <zhoro.dimitrov12@gmail.com>
2025-10-20 10:57:37 +0300

    feat: Adds a new version of the messages which provides more detailed information about the configurations

--- src/One.Settix.RabbitMQ/SettixConfigurationMessageProcessors/ConfigurationRemoved.cs
+++ src/One.Settix.RabbitMQ/SettixConfigurationMessageProcessors/ConfigurationRemoved.cs
@@ -41,3 +41,43 @@ public sealed class ConfigurationRemoved : IInterServiceConfigurable
 
     public string DestinationService => RequestPayload.ServiceKeyToReplyBack;
 }
+
+[DataContract(Name = ContractId)]
+public sealed class ConfigurationRemovedV2 : IInterServiceConfigurable
+{
+    internal const string ContractId = "017626a1-713a-4dbf-a4da-9ae9d7c27b79";
+
+    ConfigurationRemovedV2()
+    {
+        Data = new HashSet<ConfigureServiceData>();
+    }
+
+    public ConfigurationRemovedV2(string tenant, RemoveConfigurationV2 requestPayload, bool isRestartRequired, HashSet<ConfigureServiceData> data, bool isSuccess, DateTimeOffset timestamp)
+    {
+        if (tenant != requestPayload.Tenant)
+            throw new ArgumentException("Tenant mismatch");
+
+        Tenant = tenant;
+        RequestPayload = requestPayload;
+        IsRestartRequired = isRestartRequired;
+        Data = data;
+        IsSuccess = isSuccess;
+        Timestamp = timestamp;
+    }
+
+    public string Tenant { get; private set; }
+
+    public RemoveConfigurationV2 RequestPayload { get; private set; }
+
+    public bool IsRestartRequired { get; private set; }
+
+    public HashSet<ConfigureServiceData> Data { get; private set; }
+
+    public bool IsSuccess { get; private set; }
+
+    public DateTimeOffset Timestamp { get; private set; }
+
+    public string Contract => ContractId;
+
+    public string DestinationService => RequestPayload.ServiceKeyToReplyBack;
+}
