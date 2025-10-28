6dd04cf5ca7c83426137115f766b9f1788eb1013
Georgi Shukov <georgi.shukov98@gmail.com>
2025-10-28 14:45:32 +0200

    fix: Reverts all dynamic configuration changes

--- src/One.Settix.RabbitMQ/SettixConfigurationMessageProcessors/ConfigureService.cs
+++ src/One.Settix.RabbitMQ/SettixConfigurationMessageProcessors/ConfigureService.cs
@@ -35,37 +35,3 @@ public sealed class ConfigureService : IInterServiceConfigurable
 
     public string DestinationService => ServiceKeyToConfigure;
 }
-
-[DataContract(Name = ContractId)]
-public sealed class ConfigureServiceV2 : IInterServiceConfigurable
-{
-    internal const string ContractId = "4928137f-cc54-4470-99e4-0aed21cb57cf";
-
-    ConfigureServiceV2()
-    {
-        Data = new Dictionary<string, object>();
-    }
-
-    public ConfigureServiceV2(string tenant, string serviceKeyToConfigure, string serviceKeyToReplyBack, Dictionary<string, object> data, DateTimeOffset timestamp)
-    {
-        Tenant = tenant;
-        ServiceKeyToConfigure = serviceKeyToConfigure;
-        ServiceKeyToReplyBack = serviceKeyToReplyBack;
-        Data = data ?? new Dictionary<string, object>();
-        Timestamp = timestamp;
-    }
-
-    public string Tenant { get; private set; }
-
-    public string ServiceKeyToConfigure { get; private set; }
-
-    public string ServiceKeyToReplyBack { get; private set; }
-
-    public Dictionary<string, object> Data { get; private set; }
-
-    public DateTimeOffset Timestamp { get; private set; }
-
-    public string Contract => ContractId;
-
-    public string DestinationService => ServiceKeyToConfigure;
-}
--- src/One.Settix.RabbitMQ/SettixConfigurationMessageProcessors/IPandoraConfigurationMessageProcessor.cs
+++ src/One.Settix.RabbitMQ/SettixConfigurationMessageProcessors/IPandoraConfigurationMessageProcessor.cs
@@ -2,13 +2,8 @@
 
 public interface ISettixConfigurationMessageProcessor // TODO: this needs to be thought over and changed. This interface handles 2 types of messages. This is not a real case, because you either want to consume 1 or the other...
 {
-    Task ProcessAsync(ConfigureService message) => Task.CompletedTask;
+    Task ProcessAsync(ConfigureService message);
     Task ProcessAsync(ServiceConfigured message) => Task.CompletedTask;
-    Task ProcessAsync(RemoveConfiguration message) => Task.CompletedTask;
+    Task ProcessAsync(RemoveConfiguration message);
     Task ProcessAsync(ConfigurationRemoved message) => Task.CompletedTask;
-
-    Task ProcessAsync(ConfigureServiceV2 message);
-    Task ProcessAsync(ServiceConfiguredV2 message) => Task.CompletedTask;
-    Task ProcessAsync(RemoveConfigurationV2 message);
-    Task ProcessAsync(ConfigurationRemovedV2 message) => Task.CompletedTask;
 }
